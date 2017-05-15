fdeab49bbca3137925616bebf5a75c3b82164f16
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-05-15 15:44:49 +0200

    add quick fix for some words not finding

--- commands/pronounce.py
+++ commands/pronounce.py
@@ -6,13 +6,15 @@ from bs4 import BeautifulSoup
 
 
 def run(bot, chat_id, user, keyConfig='', requestText='', totalResults=1):
-    rawMarkup = urllib.urlopen('https://www.dwds.de/?from=wb&q=' + requestText).read()
+    full_url = 'https://www.dwds.de/?from=wb&q=' + requestText.title()
+    print full_url
+    rawMarkup = urllib.urlopen(full_url).read()
     soup = BeautifulSoup(rawMarkup, 'html.parser')
     rawAudioSourceTag = soup.find('source', attrs={'type':'audio/mpeg'})
     if rawAudioSourceTag:
         bot.sendMessage(chat_id=chat_id, text='http:' + rawAudioSourceTag['src'])
     else:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') + \
-                                              ', I\'m afraid I can\'t find any pronunciations for ' + \
+                                              ', I\'m afraid I can\'t find any pronunciations of ' + \
                                               requestText.encode('utf-8'))
 
--- tests/test_pronounce.py
+++ tests/test_pronounce.py
@@ -8,7 +8,7 @@ import commands.pronounce as pronounce
 
 class TestTorrent(unittest.TestCase):
     def test_translate(self):
-        requestText = 'schlacken'
+        requestText = 'schwanz'
 
         keyConfig = ConfigParser.ConfigParser()
         keyConfig.read(["keys.ini", "..\keys.ini"])
