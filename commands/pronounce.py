a0e59620ce8aff5ea29895819976f26c6b655cca
Salami Army <ultrakiff@gmail.com>
2017-05-27 19:36:01 +0200

    titale case fix for weird /pronounce search behaviour (title case changes the search results)

--- commands/pronounce.py
+++ commands/pronounce.py
@@ -6,22 +6,32 @@ from bs4 import BeautifulSoup
 
 
 def run(bot, chat_id, user, keyConfig='', requestText='', totalResults=1):
-    args = {'from': 'wb',
-            'q': requestText.title()}
-    full_url = 'https://www.dwds.de/' + '?' + urllib.urlencode(args)
-    print full_url
-    rawMarkup = urllib.urlopen(full_url).read()
-    soup = BeautifulSoup(rawMarkup, 'html.parser')
-    rawAudioSourceTag = soup.find('source', attrs={'type':'audio/mpeg'})
+    rawAudioSourceTag, error = search_pronounciations(requestText)
     if rawAudioSourceTag:
         bot.sendMessage(chat_id=chat_id, text='http:' + rawAudioSourceTag['src'])
     else:
-        error = soup.find('p', attrs={'class':'bg-danger'})
-        if error:
-            bot.sendMessage(chat_id=chat_id, text='Es tut mir Leid ' + (user if not user == '' else 'Dave') +
-                                                  ', ' + error.text)
+        rawAudioSourceTag, error = search_pronounciations(requestText, False)
+        if rawAudioSourceTag:
+            bot.sendMessage(chat_id=chat_id, text='http:' + rawAudioSourceTag['src'])
         else:
-            bot.sendMessage(chat_id=chat_id, text='Es tut mir Leid ' + (user if not user == '' else 'Dave') + \
-                                                  ', Ich habe angst, dass ich keine aussprache von finden kann ' + \
-                                                  requestText.encode('utf-8') + '.')
+            if error:
+                bot.sendMessage(chat_id=chat_id, text='Es tut mir Leid ' + (user if not user == '' else 'Dave') +
+                                                      ', ' + error.text)
+            else:
+                bot.sendMessage(chat_id=chat_id, text='Es tut mir Leid ' + (user if not user == '' else 'Dave') + \
+                                                      ', Ich habe angst, dass ich keine aussprache von finden kann ' + \
+                                                      requestText.encode('utf-8') + '.')
+
+
+def search_pronounciations(requestText, titleCase=True):
+    if titleCase:
+        requestText = requestText.title()
+    args = {'from': 'wb',
+            'q': requestText}
+    full_url = 'https://www.dwds.de/' + '?' + urllib.urlencode(args)
+    rawMarkup = urllib.urlopen(full_url).read()
+    soup = BeautifulSoup(rawMarkup, 'html.parser')
+    rawAudioSourceTag = soup.find('source', attrs={'type': 'audio/mpeg'})
+    error = soup.find('p', attrs={'class':'bg-danger'})
+    return rawAudioSourceTag, error
 
