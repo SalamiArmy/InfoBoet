2bb2a18f3941e4a02cb528e717ff5da1c7ba33cc
Salami Army <ultrakiff@gmail.com>
2017-05-29 18:20:20 +0200

    ccant check if beautiful soup tag contain attribute

--- commands/pronounce.py
+++ commands/pronounce.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 
 def run(bot, chat_id, user, keyConfig='', requestText='', totalResults=1):
     rawAudioSourceTag, error, full_url = search_pronounciations(requestText)
-    if rawAudioSourceTag and 'src' in rawAudioSourceTag:
-        bot.sendMessage(chat_id=chat_id, text='http:' + rawAudioSourceTag['src'])
+    if rawAudioSourceTag:
+        bot.sendMessage(chat_id=chat_id, text='http:' + rawAudioSourceTag)
     else:
         if error and error.text:
             bot.sendMessage(chat_id=chat_id, text='Es tut mir Leid ' + (user if not user == '' else 'Dave') +
@@ -20,8 +20,8 @@ def run(bot, chat_id, user, keyConfig='', requestText='', totalResults=1):
 
 
 def search_pronounciations(requestText):
-    rawAudioSourceTag, error, full_url = search_impl(requestText)
-    if rawAudioSourceTag and 'src' in rawAudioSourceTag:
+    rawAudioSourceTag, error, full_url = search_impl(requestText.lower())
+    if rawAudioSourceTag:
         return rawAudioSourceTag, error, full_url
     else:
         rawAudioSourceTag, error, full_url = search_impl(requestText.title())
@@ -34,7 +34,8 @@ def search_impl(requestText):
     full_url = 'https://www.dwds.de/' + '?' + urllib.urlencode(args)
     rawMarkup = urllib.urlopen(full_url).read()
     soup = BeautifulSoup(rawMarkup, 'html.parser')
-    rawAudioSourceTag = soup.find('source', attrs={'type': 'audio/mpeg'})
+    getAudioTag = soup.find('source', attrs={'type': 'audio/mpeg'})
+    rawAudioSourceTag = getAudioTag['src'] if getAudioTag else None
     error = soup.find('p', attrs={'class': 'bg-danger'})
     return rawAudioSourceTag, error, full_url
 
