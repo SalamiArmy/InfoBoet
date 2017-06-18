8515c285ba3ee41e2e8e0c08a29bf34b0082cf0a
Salami Army <ultrakiff@gmail.com>
2017-06-18 22:07:14 +0200

    fix /reverseimage

--- tests/test_reverseimage.py
+++ tests/test_reverseimage.py
@@ -3,9 +3,27 @@ import unittest
 import telegram
 
 import commands.reverseimage as reverseimage
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
 
 
 class TestReverseImage(unittest.TestCase):
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_user_stub()
+        self.testbed.init_urlfetch_stub()
+        # Clear ndb's in-context cache between tests.
+        # This prevents data from leaking between tests.
+        # Alternatively, you could disable caching by
+        # using ndb.get_context().set_cache_policy(False)
+        ndb.get_context().clear_cache()
+
     def test_reverseimage(self):
         requestText = 'https://s-media-cache-ak0.pinimg.com/originals/e3/68/e2/e368e259e24b47d3b61eeed92a2e1b0d.gif'
 
