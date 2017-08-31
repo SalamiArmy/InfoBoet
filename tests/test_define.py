cbc3a7f5b92dd1dbedfa847126ae1a669340a7c1
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-08-31 08:44:37 +0200

    fiddle with /define its not working or logging any errors go figure

--- tests/test_define.py
+++ tests/test_define.py
@@ -9,11 +9,11 @@ import commands.define as define
 
 class TestDefine(unittest.TestCase):
     def test_define(self):
-        requestText = 'tetragrammaton'
+        requestText = 'safe'
 
         keyConfig = ConfigParser.ConfigParser()
         keyConfig.read(["keys.ini", "..\keys.ini"])
         bot = telegram.Bot(keyConfig.get('Telegram', 'TELE_BOT_ID'))
         chatId = keyConfig.get('BotAdministration', 'TESTING_GROUP_CHAT_ID')
 
-        define.run(bot, chatId, 'Admin', keyConfig, requestText)
+        define.run(bot, chatId, 'SalamiArmy', keyConfig, requestText)
