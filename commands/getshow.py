4d66b0105654bd7bb805a85a9a0905b843f15503
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-09-07 10:57:29 +0200

    /getshow handles no show image gracefully

--- commands/getshow.py
+++ commands/getshow.py
@@ -13,19 +13,21 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     showsUrl = 'http://api.tvmaze.com/search/shows?q='
     data = json.load(urllib.urlopen(showsUrl + requestText))
     if len(data) >= 1:
-        formattedShowSnippet = re.sub(r'<[^>]*?>', '',
+        formattedShowSummary = re.sub(r'<[^>]*?>', '',
                                       data[0]['show']['summary']
                                        .replace('<span class="searchmatch">', '*')
                                        .replace('</span>', '*')
                                        .replace('&quot;', '\"')
                                        .replace('[', '')
                                        .replace(']', '')
-                                       .replace('\\', '')[:125].strip())
-        bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
-        image_original = data[0]['show']['image']['original'].encode('utf-8')
-        bot.sendPhoto(chat_id=chat_id,
-                      photo=image_original,
-                      caption=formattedShowSnippet)
+                                       .replace('\\', ''))
+        if 'image' in data[0]['show'] and data[0]['show']['image'] is not None:
+            bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
+            image_original = data[0]['show']['image']['original'].encode('utf-8')
+            bot.sendPhoto(chat_id=chat_id,
+                          photo=image_original)
+        bot.sendMessage(chat_id=chat_id,
+                        text=(user if not user == '' else 'Dave') + ', ' + data[0]['show']['name'] + ': ' + formattedShowSummary)
         return True
     else:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') + \
