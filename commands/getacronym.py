eb4b62c6f604554ba400eef90c884a2d859829c5
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-06 14:34:46 +0200

    fix for /getacronym

--- commands/getacronym.py
+++ commands/getacronym.py
@@ -13,7 +13,7 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     resultsList = acronym_results_parser(code)
     if resultsList:
         searchResults = acronym_results_printer(requestText, resultsList)
-        bot.sendMessage(chat_id=chat_id, text=user + ', ' + searchResults.replace('***', ''),
+        bot.sendMessage(chat_id=chat_id, text=user + ', ' + searchResults,
                         disable_web_page_preview=True, parse_mode='Markdown')
         return True
     else:
@@ -30,12 +30,12 @@ def acronym_results_parser(code):
     return resultList
 
 def acronym_results_printer(request, list):
-    AllGameDetailsFormatted= '*' + str(request) + '* could mean:'
+    AllGameDetailsFormatted = '*' + str(request) + '* could mean:'
     for item in list:
         encodedItem = item.encode('utf-8')
         if (encodedItem != 'None'):
             AllGameDetailsFormatted += '\n'
-            for char in encodedItem.replace('Definition', ''):
+            for char in encodedItem.replace('Definition', '').replace('***', ''):
                 if char.isupper():
                     AllGameDetailsFormatted += '*' + char + '*'
                 else:
