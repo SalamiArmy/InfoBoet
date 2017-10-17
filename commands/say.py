72552d3167569e65baffa0a4915628b623d97cc3
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-17 11:54:35 +0200

    update /say

--- commands/say.py
+++ commands/say.py
@@ -7,24 +7,20 @@ from google.appengine.api import urlfetch
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = message.encode('utf-8')
     if '<voice-transformation' in requestText or '<express-as' in requestText:
-        voice = 'en-US_AllisonVoice'
+        voices = ['en-US_AllisonVoice']
     else:
-        voice = 'en-US_LisaVoice'
+        voices = ['en-US_LisaVoice']
     sent = False
-    if not send_text_as_voice(chat_id, keyConfig, requestText, sent, voice):
+    for voice in voices:
+        data = get_voice(requestText, keyConfig, voice)
+        if data:
+            requests.post('https://api.telegram.org/bot' + keyConfig.get('Telegram', 'TELE_BOT_ID') +
+                          '/sendVoice?chat_id='+str(chat_id),
+                          files={'voice': ('no but what I\'M saying is.ogg', data, 'audio/ogg', {'Expires': '0'})})
+            sent = True
+    if not sent:
         bot.sendMessage(chat_id=str(chat_id), text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                   ', I\'m afraid I can\'t say that.')
-
-
-def send_text_as_voice(chat_id, keyConfig, requestText, voice):
-    data = get_voice(requestText, keyConfig, voice)
-    if data:
-        requests.post('https://api.telegram.org/bot' + keyConfig.get('Telegram', 'TELE_BOT_ID') +
-                      '/sendVoice?chat_id=' + str(chat_id),
-                      files={'voice': ('no but what I\'M saying is.ogg', data, 'audio/ogg', {'Expires': '0'})})
-        return True
-    return False
-
 
 def get_voice(text, keyConfig, voice):
     IBMusername = keyConfig.get('IBM', 'username')
