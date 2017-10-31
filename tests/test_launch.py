06e6ae93d48d62b925ab960254373286a91a73c4
Salami Army <ultrakiff@gmail.com>
2017-10-31 18:55:06 +0200

    update test for /launch to scenic oxygen

--- tests/test_launch.py
+++ tests/test_launch.py
@@ -9,7 +9,8 @@ class TestLaunch(unittest.TestCase):
     def test_launch(self):
         keyConfig = ConfigParser.ConfigParser()
         keyConfig.read(["keys.ini", "..\keys.ini"])
-        bot = telegram.Bot(keyConfig.get('Telegram', 'TELE_BOT_ID'))
-        chatId = keyConfig.get('BotAdministration', 'TESTING_ALT_GROUP_CHAT_ID')
+        keyConfig.read(["bot_keys.ini", "..\\bot_keys.ini"])
+        bot = telegram.Bot(keyConfig.get('BotIDs', 'TELEGRAM_BOT_ID'))
+        chatId = keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_PRIVATE_CHAT_ID')
 
         launch.run(bot, chatId, 'Admin', keyConfig, '')
