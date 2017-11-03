dbccec97d23223deb2044522c2276be595a7c7eb
Ashley Lewis <ultrakiff@gmail.com>
2017-11-03 20:17:55 +0200

    simplify join operation after getting all commands

--- commands/help.py
+++ commands/help.py
@@ -9,7 +9,7 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
         if len(es) > 0:
             for mod in es:
                 available_commands.append(str(mod.key._Key__pairs[0][1]))
-        bot.sendMessage(chat_id=chat_id, text="I know:\n" + "\n".join(map(lambda x: x[:-3], available_commands)))
+        bot.sendMessage(chat_id=chat_id, text="I know:\n" + "\n".join(available_commands))
         return True
     except:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
