db290c489caef6a85a77484e05bc999e4ceeb1ba
Salami Army <ultrakiff@gmail.com>
2017-11-24 17:39:31 +0200

    deprecate bot and keyConfig arguments /getmovie

--- commands/getmovie.py
+++ commands/getmovie.py
@@ -2,25 +2,19 @@
 import json
 import urllib
 
-import telegram
 
-
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    requestText = message.replace(bot.name, "").strip()
+def run(user, message, chat_id='', totalResults=1):
+    requestText = str(message).strip()
 
     movieUrl = 'http://www.omdbapi.com/?apikey=' + keyConfig.get('OMDB', 'KEY') + '&plot=short&r=json&y=&t='
     realUrl = movieUrl + requestText.encode('utf-8')
     data = json.load(urllib.urlopen(realUrl))
     if 'Error' not in data:
+        result = (user + ', ' if not user == '' else '') + data['Title'] + ':\n' + data['Plot']
         if 'Poster' in data and not data['Poster'] == 'N/A':
-            bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
-            bot.sendPhoto(chat_id=chat_id, photo=data['Poster'].encode('utf-8'),
-                          caption=(user+ ', ' if not user == '' else '') + data['Title'] + ':\n' + data['Plot'][400:])
-            return True
-        else:
-            bot.sendMessage(chat_id=chat_id, text=(user + ', ' if not user == '' else '') +
-                                      data['Title'] + ':\n' + data['Plot'])
+            result += '\n' + data['Poster'].encode('utf-8')
+        return result
     else:
-        bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
-                                              ', I\'m afraid I can\'t find any movies for ' +
-                                              requestText.encode('utf-8') + '.')
+        return 'I\'m sorry ' + (
+        user if not user == '' else 'Dave') + ', I\'m afraid I can\'t find any movies for ' + \
+               requestText.encode('utf-8') + '.'
