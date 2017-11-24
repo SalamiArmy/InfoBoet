5ebeaeaf76eafd623c3a6a20e52e8eee3a6aedaf
Salami Army <ultrakiff@gmail.com>
2017-11-24 17:26:57 +0200

    deprecate bot and keyCOnfig arguments in /urban

--- commands/urban.py
+++ commands/urban.py
@@ -5,19 +5,19 @@ import string
 import urllib
 
 
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    requestText = message.replace(bot.name, "").strip()
+def run(user, message, chat_id='', totalResults=1):
+    requestText = str(message).strip()
 
     dicurl = 'http://api.urbandictionary.com/v0/define?term='
     realUrl = dicurl + requestText.encode('utf-8')
     data = json.load(urllib.urlopen(realUrl))
     if 'list' in data and len(data['list']) >= 1:
         resultNum = data['list'][random.randint(0, len(data['list']) - 1)]
-        bot.sendMessage(chat_id=chat_id, text=(user + ': ' if not user == '' else '') +\
-                                              'Urban Definition For ' + string.capwords(requestText.encode('utf-8')) + ":\n" + resultNum['definition'] +\
-                                              '\n\nExample:\n' + resultNum['example'])
-        return True
+        result = (user + ': ' if not user == '' else '') + 'Urban Definition For ' + string.capwords(
+            requestText.encode('utf-8')) + ':\n' + resultNum['definition'] + '\n\nExample:\n' + resultNum['example']
+        return result
     else:
-        bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +\
-                                              ', I\'m afraid I can\'t find any urban definitions for ' +\
-                                              string.capwords(requestText.encode('utf-8')) + '.')
+        result = 'I\'m sorry ' + (user if not user == '' else 'Dave') + \
+                 ', I\'m afraid I can\'t find any urban definitions for ' + string.capwords(
+            requestText.encode('utf-8')) + '.'
+        return result
