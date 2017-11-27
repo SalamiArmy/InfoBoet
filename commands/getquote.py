326c9a6c301472c82e320eef9030890359c07fcc
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-27 08:12:45 +0200

    refactor /getquote markdown parsing into scenic oxygen

--- commands/getquote.py
+++ commands/getquote.py
@@ -9,28 +9,19 @@ def run(user, message, chat_id='', totalResults=1):
 
     data = wiki_search('%22' + requestText + '%22')
     if len(data['query']['search']) >= 1:
-        return formated_wiki_result(data, user)
+        return (user + ': ' if not user == '' else '') + data['query']['search'][0]['snippet'] +\
+               '\nhttps://en.wikiquote.org/wiki/' +\
+               urllib.quote(data['query']['search'][0]['title'].encode('utf-8'))
     else:
         data = wiki_search(requestText)
         if len(data['query']['search']) >= 1:
-            return formated_wiki_result(data, user)
+            return (user + ': ' if not user == '' else '') + data['query']['search'][0]['snippet'] +\
+           '\nhttps://en.wikiquote.org/wiki/' +\
+           urllib.quote(data['query']['search'][0]['title'].encode('utf-8'))
         else:
             return 'I\'m sorry ' + (user if not user == '' else 'Dave') +\
                    ', I\'m afraid I can\'t find any quotes for ' +\
                    requestText.encode('utf-8') + '.'
-
-
-def formated_wiki_result(data, user):
-    formattedQuoteSnippet = str(re.sub(r'<[^>]*?>', '',
-                                   data['query']['search'][0]['snippet']
-                                   .replace('<span class="searchmatch">', '*')
-                                   .replace('</span>', '*')
-                                   .replace('&quot;', '\"')
-                                   .replace('[', '')
-                                   .replace(']', '')))
-    return (user + ': ' if not user == '' else '') + formattedQuoteSnippet +\
-           '\nhttps://en.wikiquote.org/wiki/' +\
-           urllib.quote(data['query']['search'][0]['title'].encode('utf-8'))
 
 
 def wiki_search(requestText):
