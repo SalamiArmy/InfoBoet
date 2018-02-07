e93df25daf68cd6bbb1c581841ed3dcdde889a7e
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-02-07 08:07:39 +0200

    still trying to avoid index out of range exception here

--- commands/wiki.py
+++ commands/wiki.py
@@ -21,7 +21,7 @@ def run(user, message, chat_id='', totalResults=1):
             'https://en.wikipedia.org/w/api.php?action=opensearch&namespace=0&format=json&search='
         realUrl = wikiUrl + requestText.encode('utf-8')
         data = json.load(urllib.urlopen(realUrl))
-        while int(total_sent) < len(data[2]) and int(total_sent) < int(totalResults):
+        while len(data) > 2 and int(total_sent) < len(data[2]) and int(total_sent) < int(totalResults):
             result += ('\n' if result != '' else '') + (user + ': ' if not user == '' else '') + \
                       data[2][total_sent] + '\nLink: ' + data[3][total_sent]
             total_sent += 1
