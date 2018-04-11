df710370cdef6e9e4cc9422116292b2356d1c06e
Ashley Lewis <ultrakiff@gmail.com>
2018-04-11 08:25:14 +0200

    still logging /say

--- commands/say.py
+++ commands/say.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import base64
 import json
 import urllib
@@ -6,7 +7,9 @@ import requests
 from google.appengine.api import urlfetch
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    requestText = message.encode('utf-8')
+    requestText = str(message)
+    #logging.info('Saying: ' + requestText)
+    print 'Saying: ' + requestText
     languageCode = 'en-GB'
     voice = 'Standard-A'
     if not send_text_as_voice(chat_id, keyConfig, requestText, voice, languageCode):
