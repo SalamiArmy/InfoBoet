884022a6e7eb036be7e66ec336fc63819b30e388
Ashley Lewis <ultrakiff@gmail.com>
2018-04-18 16:39:36 +0200

    settling on a voice for /say

--- commands/say.py
+++ commands/say.py
@@ -7,18 +7,10 @@ from google.appengine.api import urlfetch
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = str(message)
-    fullVoice = ['en-AU-Standard-A',	
-                 'en-AU-Standard-B',	
-                 'en-AU-Standard-C',	
-                 'en-AU-Standard-D',
-                 'en-GB-Standard-A',
-                 'en-GB-Standard-B',
-                 'en-GB-Standard-C',
-                 'en-GB-Standard-D']
+    fullVoice = ['en-GB-Standard-A']
     for voice in fullVoice:
         languageCode = voice.split('-')[0] + '-' + voice.split('-')[1]
         voice = voice.split('-')[2] + '-' + voice.split('-')[3]
-        bot.sendMessage(chat_id=str(chat_id), text=str(languageCode) + "-" + str(voice) + ':')
         if not send_text_as_voice(chat_id, keyConfig, requestText, voice, languageCode):
             bot.sendMessage(chat_id=str(chat_id), text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                       ', I\'m afraid I can\'t say that.')
