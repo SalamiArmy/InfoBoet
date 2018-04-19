39beabd494ac5c7514d75b1fefdb6d847de6c92d
Ashley Lewis <ultrakiff@gmail.com>
2018-04-19 13:57:50 +0200

    Update bitcoin.py

--- commands/bitcoin.py
+++ commands/bitcoin.py
@@ -3,28 +3,13 @@ import json
 
 from google.appengine.api import urlfetch
 
-def run(bot, chat_id, user, keyConfig, message="", totalResults=1):
-    tickers = ['BTC',
-               'ETH',
-               'XRP',
-               'BCH',
-               'LTC',
-               'EOS',
-               'ADA',
-               'XLM',
-               'NEO',
-               'IOTA',
-               'XMR',
-               'XEM',
-               'DASH',
-               'TRX']
-    for ticker in tickers:
-        bcurl = 'https://min-api.cryptocompare.com/data/price?fsym=' + ticker + '&tsyms=ZAR,USD,EUR'
-        RAW_DATA = urlfetch.fetch(bcurl)
-        data = json.loads(RAW_DATA.content)
-        bot.sendMessage(chat_id=chat_id,
-                        text='The Current Price of 1 ' + ticker + ':\n\n' + str(data['USD']) +
-                             ' USD\n' + str(data['EUR']) +
-                             ' EUR\n' + str(data['ZAR']) + ' ZAR')
+def run(bot, chat_id, user, keyConfig, message="BTC", totalResults=1):
+    bcurl = 'https://min-api.cryptocompare.com/data/price?fsym=' + message + '&tsyms=ZAR,USD,EUR'
+    RAW_DATA = urlfetch.fetch(bcurl)
+    data = json.loads(RAW_DATA.content)
+    bot.sendMessage(chat_id=chat_id,
+                    text='The Current Price of 1 ' + ticker + ':\n\n' + str(data['USD']) +
+                         ' USD\n' + str(data['EUR']) +
+                         ' EUR\n' + str(data['ZAR']) + ' ZAR')
     return True
         
