f92854e88483fd1cf735c71a962224acf81b8905
Ashley Lewis <ultrakiff@gmail.com>
2018-04-19 15:23:15 +0200

    improve /bitcoin error handling

--- commands/bitcoin.py
+++ commands/bitcoin.py
@@ -1,18 +1,19 @@
 # coding=utf-8
 import json
-import logging
 
 from google.appengine.api import urlfetch
 
 def run(bot, chat_id, user, keyConfig, message="BTC", totalResults=1):
     bcurl = 'https://min-api.cryptocompare.com/data/price?fsym=' + message + '&tsyms=ZAR,USD,EUR'
     RAW_DATA = urlfetch.fetch(bcurl)
-    logging.info('Bitcoin content:')
-    logging.info(RAW_DATA.content)
     data = json.loads(RAW_DATA.content)
-    bot.sendMessage(chat_id=chat_id,
-                    text='The Current Price of 1 ' + message + ':\n\n' + str(data['USD']) +
-                         ' USD\n' + str(data['EUR']) +
-                         ' EUR\n' + str(data['ZAR']) + ' ZAR')
+    if 'USD' in data and 'EUR' in data and 'ZAR' in data:
+        bot.sendMessage(chat_id=chat_id,
+                        text='The Current Price of 1 ' + message + ':\n\n' + str(data['USD']) +
+                             ' USD\n' + str(data['EUR']) +
+                             ' EUR\n' + str(data['ZAR']) + ' ZAR')
+    else:
+        bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
+                                              ', ' + data['Message'])
     return True
         
