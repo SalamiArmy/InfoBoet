e305b4fd7053cacf4534f0f466696d7cd6278996
Ashley Lewis <ultrakiff@gmail.com>
2018-04-20 15:17:46 +0200

    Delete bitcoin.py

--- commands/bitcoin.py
+++
@@ -1,4 +0,0 @@
-import main
-getfig = main.load_code_as_module('getfig')
-def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    getfig.run(bot, chat_id, user, keyConfig, message, totalResults)
