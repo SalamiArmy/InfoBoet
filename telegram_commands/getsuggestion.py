aeb0dc3deae21220b9c452302bb7742e2f04c9fc
Ashley Lewis <ultrakiff@gmail.com>
2018-05-11 19:35:56 +0200

    catch this error

--- telegram_commands/getsuggestion.py
+++ telegram_commands/getsuggestion.py
@@ -11,7 +11,10 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     data = urllib.urlencode(values)
     req = urllib2.Request(url + '?' + data)
     req.add_header('Ocp-Apim-Subscription-Key', keyConfig.get('Bing', 'AutoSuggestApiKey'))
-    data = json.load(urllib2.urlopen(req))
+    try:
+        data = json.load(urllib2.urlopen(req))
+    except urllib2.HTTPError:
+        print('HTTP Error: ' + url + '?' + data)
 
     if 'suggestionGroups' in data and len(data['suggestionGroups']) >= 1 and 'searchSuggestions' in data['suggestionGroups'][0] and len(data['suggestionGroups'][0]['searchSuggestions']) >= 1 and 'displayText' in data['suggestionGroups'][0]['searchSuggestions'][0] and data['suggestionGroups'][0]['searchSuggestions'][0]['displayText'] != requestText:
         returnText = ''
