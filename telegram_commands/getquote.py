5cbb142486cc3283f77eb88ddf7d36d671c8d927
Ashley Lewis <ultrakiff@gmail.com>
2018-05-22 13:29:40 +0200

    improve /getquote message format

--- telegram_commands/getquote.py
+++ telegram_commands/getquote.py
@@ -21,7 +21,7 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
             result = 'I\'m sorry ' + (user if not user == '' else 'Dave') +\
                    ', I\'m afraid I can\'t find any quotes for ' +\
                    requestText.encode('utf-8') + '.'
-    bot.sendMessage(chat_id=chat_id, text=result)
+    bot.sendMessage(chat_id=chat_id, text=result.replace('<span class="searchmatch">', '*').replace('</span>', '*'), parse_mode='Markdown')
 
 
 def wiki_search(requestText):
