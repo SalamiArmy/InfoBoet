573df0b4651bd0060a68c983c8a16a37285b2abd
Ashley Lewis <ashley.lewis@dev2.co.za>
2018-06-28 11:33:35 +0200

    update /getflight web command

--- web_commands/getflight.py
+++ web_commands/getflight.py
@@ -7,18 +7,17 @@ telegramgetflight = main.get_platform_command_code('telegram', 'getflight')
 
 def run(keyConfig, message, totalResults=1):
     requestText = message.strip()
-    if (requestText[3] == " " and requestText[7] == " " and requestText[12] == "-" and requestText[15] == "-"):
-        flightData = telegramgetflight.get_flights(requestText)
-        if flightData != '':
-            return flightData
-        else:
-            return 'I\'m sorry Dave, I\'m afraid I can\'t find any one way flights for ' + str(requestText) + '.'
+    if (requestText[3] == " " and requestText[7] == " " and requestText[12] == "-" and requestText[15] == "-" and requestText[18] == " " and requestText[23] == "-" and requestText[26] == "-"):
+        return telegramgetflight.get_returnflights(requestText)
     else:
-        airportCode, error = telegramgetflight.get_airport_code(requestText)
-        if airportCode != 'No matching entries found...':
-            return airportCode
+        if (requestText[3] == " " and requestText[7] == " " and requestText[12] == "-" and requestText[15] == "-"):
+            return telegramgetflight.get_flights(requestText)
         else:
-            if error:
-                return 'I\'m sorry Dave' + error
+            airportCode, error = telegramgetflight.get_airport_code(requestText)
+            if airportCode != 'No matching entries found...':
+                return airportCode
             else:
-                return 'I\'m sorry Dave, I\'m afraid I can\'t quite place ' + str(requestText) + '.'
+                if error:
+                    return 'I\'m sorry Dave' + error
+                else:
+                    return 'I\'m sorry Dave, I\'m afraid I can\'t quite place ' + str(requestText) + '.'
