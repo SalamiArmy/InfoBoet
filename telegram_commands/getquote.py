09b3686679c3a5dc656ed3a75256be107845ea9b
Ashley Lewis <ultrakiff@gmail.com>
2018-07-16 13:08:14 +0200

    disable markdown in /getquote again

--- telegram_commands/getquote.py
+++ telegram_commands/getquote.py
@@ -25,7 +25,7 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
                    requestText.encode('utf-8') + '.'
     bot.sendMessage(chat_id=chat_id, text=result
                     .replace('<span class="searchmatch">', '*')
-                    .replace('</span>', '*'), parse_mode='Markdown')
+                    .replace('</span>', '*'))
 
 
 def wiki_search(requestText):
