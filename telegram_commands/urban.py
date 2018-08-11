d53e51aefb8a5486138b15d27e77313e24266ab6
Ashley Lewis <ultrakiff@gmail.com>
2018-08-11 08:46:38 +0200

    trying markdown parsing for /urban command

--- telegram_commands/urban.py
+++ telegram_commands/urban.py
@@ -13,10 +13,9 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     data = json.load(urllib.urlopen(realUrl))
     if 'list' in data and len(data['list']) >= 1:
         resultNum = data['list'][random.randint(0, len(data['list']) - 1)]
-        result = (user + ': ' if not user == '' else '') + 'Urban Definition For ' + string.capwords(
-            requestText.encode('utf-8')) + ':\n' + resultNum['definition'] + '\n\nExample:\n' + resultNum['example']
+        result = (user + ': ' if not user == '' else '') + \
+                 'Urban Definition For ' + str(requestText) + ':\n' + resultNum['definition'] + '\n\nExample:\n' + resultNum['example']
     else:
         result = 'I\'m sorry ' + (user if not user == '' else 'Dave') + \
-                 ', I\'m afraid I can\'t find any urban definitions for ' + string.capwords(
-            requestText.encode('utf-8')) + '.'
-    bot.sendMessage(chat_id=chat_id, text=result)
+                 ', I\'m afraid I can\'t find any urban definitions for ' + str(requestText) + '.'
+    bot.sendMessage(chat_id=chat_id, text=result, parse_mode='Markdown')
