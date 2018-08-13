d7ebc0210cd435f78f1a3c643ee4d28178c16809
Ashley Lewis <ultrakiff@gmail.com>
2018-08-13 18:26:56 +0200

    try catch markdown errors in /urban

--- telegram_commands/urban.py
+++ telegram_commands/urban.py
@@ -18,4 +18,7 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     else:
         result = 'I\'m sorry ' + (user if not user == '' else 'Dave') + \
                  ', I\'m afraid I can\'t find any urban definitions for ' + str(requestText) + '.'
-    bot.sendMessage(chat_id=chat_id, text=result, parse_mode='Markdown')
+    try:
+        bot.sendMessage(chat_id=chat_id, text=result, parse_mode='Markdown')
+    except BadRequest e:
+        bot.sendMessage(chat_id=chat_id, text=result)
