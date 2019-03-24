1a7da4362be3efc0eed9017583d702ba7e3e790a
Ashley Lewis <ultrakiff@gmail.com>
2019-03-24 15:54:53 +0200

    replace &quot; in /getquote responses

--- telegram_commands/getquote.py
+++ telegram_commands/getquote.py
@@ -15,10 +15,12 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
        urllib.quote(str(data['query']['search'][0]['title']))
     else:
         data = wiki_search(requestText)
+        snoppetText = data['query']['search'][0]['snippet']
         if 'query' in data and len(data['query']['search']) >= 1:
-            result = (user + ': ' if not user == '' else '') + data['query']['search'][0]['snippet']\
+            result = (user + ': ' if not user == '' else '') + snippetText\
                     .replace('[', '\[')\
-                    .replace(']', '\]') +\
+                    .replace(']', '\]')\
+                    .replace('&quot;', '"') +\
            '\nhttps://en.wikiquote.org/wiki/' +\
            urllib.quote(str(data['query']['search'][0]['title']))
         else:
