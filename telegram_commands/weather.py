dff6eabc5c27006c5e2c07273bd521f9053a96c7
Ashley Lewis <ultrakiff@gmail.com>
2019-09-06 11:59:28 +0200

    typo in new /weather command definition

--- telegram_commands/weather.py
+++ telegram_commands/weather.py
@@ -9,6 +9,6 @@ import telegram
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = message.replace(bot.name, "").strip()
-    url = 'https://sawx.co.za/clouds/south-africa-live-animated-satellite-clouds-stream-radar-rainfall-maps/south-africa-clouds-stream-live-animated-satellite-radar-weather-map.gif&uuid=' + \
+    url = 'https://sawx.co.za/clouds/south-africa-live-animated-satellite-clouds-stream-radar-rainfall-maps/south-africa-clouds-stream-live-animated-satellite-radar-weather-map.gif?uuid=' + \
           str(uuid.uuid4())
     bot.sendMessage(chat_id=chat_id, text=url)
