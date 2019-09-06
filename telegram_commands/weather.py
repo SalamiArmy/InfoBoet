ec4bb11e8b445b92ca6bbeee2f867407f3747654
Ashley Lewis <ultrakiff@gmail.com>
2019-09-06 11:42:21 +0200

    just make /weather just show a simple cloud map

--- telegram_commands/weather.py
+++ telegram_commands/weather.py
@@ -1,47 +1,14 @@
-import ConfigParser
 
-import requests
+import datetime
+import json
+import urllib
+import uuid
 
-keyConfig = ConfigParser.ConfigParser()
-keyConfig.read(["keys.ini", "..\keys.ini"])
-API_KEY = keyConfig.get('OpenWeatherMap', 'API_KEY')
+import telegram
 
-def is_zip_code(requested):
-    try:
-        int(requested)
-        return True
-    except:
-        return False
-
-# We make a call to:
-# api.openweathermap.org/data/2.5/weather?q={city name}
-def get_weather_name(city):
-    r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + API_KEY + "&units=imperial")
-    if r.status_code == 404:
-        return "I can't find that city"
-    if r.status_code > 300:
-        return "Some unknown error occurred"
-    return str(r.json()['main']['temp']) + " " + r.json()['weather'][0]['description']
-
-# api.openweathermap.org/data/2.5/weather?zip={zip code}
-def get_weather_zip(zipcode):
-    r = requests.get("http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "&APPID=" + API_KEY + "&units=imperial")
-    if r.status_code == 404:
-        return "I can't find that city"
-    if r.status_code > 300:
-        return "Some unknown error occurred"
-    return str(r.json()['main']['temp']) + " " + r.json()['weather'][0]['description']
-
-def run(thorin, incoming):
-    split = incoming.message.text.split(" ")
-    city_or_zip = str(split[split.index("weather"):])
-    if is_zip_code(city_or_zip):
-        return get_weather_zip(city_or_zip)
-    return get_weather_name(city_or_zip)
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    if is_zip_code(message):
-        bot.sendMessage(chat_id=chat_id, text=(user + ": ") if user != '' else '' + get_weather_zip(message))
-    else:
-        bot.sendMessage(chat_id=chat_id, text=(user + ": ") if user != '' else '' + get_weather_name(message))
-    return True
+    requestText = message.replace(bot.name, "").strip()
+    url = 'https://sawx.co.za/clouds/south-africa-live-animated-satellite-clouds-stream-radar-rainfall-maps/south-africa-clouds-stream-live-animated-satellite-radar-weather-map.gif&uuid=' + \
+          str(uuid.uuid4())
+    bot.sendMessage(chat_id=chat_id, text=url)
