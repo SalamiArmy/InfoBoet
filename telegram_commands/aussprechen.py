1882c13dd3df23d0dc42e1ea926a051bc60cff29
Ashley Lewis <ultrakiff@gmail.com>
2019-09-07 09:52:25 +0200

    update/aussprechen to latest Scenic Oxygen

--- telegram_commands/aussprechen.py
+++ telegram_commands/aussprechen.py
@@ -1,4 +1,4 @@
 import main
-pronounce = main.load_code_as_module('pronounce')
+pronounce = main.get_platform_command_code('telegram', 'pronounce')
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     pronounce.run(bot, chat_id, user, keyConfig, message, totalResults)
