941c430183ae650551aa4a8d1a93bafb5adeab7a
Ashley Lewis <ultrakiff@gmail.com>
2021-02-22 03:54:13 +0000

    Create /irish command

---
+++ telegram_commands/irish.py
@@ -0,0 +1,3 @@
+import telegram_commands.getirish as getgerman
+def run(bot, chat_id, user, keyConfig, message, totalResults=1):
+    getirish.run(bot, chat_id, user, keyConfig, message, totalResults)
