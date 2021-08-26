bc1ae3e889c9d8f74924e955a9043e0f973c9383
Ashley Lewis <ultrakiff@gmail.com>
2021-08-26 21:45:15 +0200

    start reintroducing /define

--- telegram_commands/define.py
+++ telegram_commands/define.py
@@ -5,5 +5,17 @@ import urllib
 import urllib2
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +\
-       ', I\'m afraid the Oxford dictionary API is no longer free and I have to find a new API for /define.')
+    requestText = str(message).strip()
+    keyConfig = ConfigParser.ConfigParser()
+    keyConfig.read(["keys.ini", "..\keys.ini"])
+    if requestText != '':
+        bot.sendMessage(chat_id=chat_id, text=get_define_data(keyConfig, user, requestText))
+    else:
+        bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +\
+           ', I\'m afraid you must provide a word for me to define. Like this: /define word')
+
+def get_define_data(keyConfig, user, requestText):
+    defineUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+    req = urllib2.Request(defineUrl + requestText)
+    definitionData = json.load(urllib2.urlopen(req))
+    return definitionData.dumps()
