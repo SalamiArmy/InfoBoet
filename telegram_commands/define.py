6fd2c8f44b95bc7129f83b08b605ae14e8d3702e
Ashley Lewis <ultrakiff@gmail.com>
2021-08-26 22:48:05 +0200

    dumps json here

--- telegram_commands/define.py
+++ telegram_commands/define.py
@@ -5,5 +5,19 @@ import urllib
 import urllib2
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
-    bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +\
-       ', I\'m afraid the Oxford dictionary API is no longer free and I have to find a new API for /define.')
+    requestText = str(message).strip()
+    if requestText != '':
+        bot.sendMessage(chat_id=chat_id, text=get_define_data(user, requestText))
+    else:
+        bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +\
+           ', I\'m afraid you must provide a word for me to define. Like this: /define word')
+
+def get_define_data(user, requestText):
+    defineUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+    req = urllib2.Request(defineUrl + requestText)
+    definitionData = json.load(urllib2.urlopen(req))
+    returnData = ''
+    length = len(definitionData)
+    for i in range(length):
+        returnData = returnData + '\n' + json.dumps(definitionData[i])        
+    return returnData
