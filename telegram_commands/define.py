020817f8c995d2823bbea3efa3846265de4bb5ce
Ashley Lewis <ultrakiff@gmail.com>
2021-08-29 06:01:10 +0200

    continue formatting /define data

--- telegram_commands/define.py
+++ telegram_commands/define.py
@@ -7,7 +7,7 @@ import urllib2
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = str(message).strip()
     if requestText != '':
-        bot.sendMessage(chat_id=chat_id, text=get_define_data(user, requestText))
+        bot.sendMessage(chat_id=chat_id, text=get_define_data(user, requestText), parse_mode='Markdown')
     else:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +\
            ', I\'m afraid you must provide a word for me to define. Like this: /define word')
@@ -22,14 +22,17 @@ def get_define_data(user, requestText):
         for i in range(length):
             returnData = returnData + '\n*' + definitionData[i]['word'] + ':*'
             if 'meanings' in definitionData[i] and len(definitionData[i]['meanings']) > 0:
+                definitionData = ''
+                synonymData = '`'
                 for j in range(len(definitionData[i]['meanings'])):
                     if 'definitions' in definitionData[i]['meanings'][j] and len(definitionData[i]['meanings'][j]['definitions']) > 0:
                         for k in range(len(definitionData[i]['meanings'][j]['definitions'])):
                             if 'definition' in definitionData[i]['meanings'][j]['definitions'][k]:
-                                returnData = returnData + '\n' + definitionData[i]['meanings'][j]['definitions'][k]['definition']
+                                definitionData = definitionData + ' ' + definitionData[i]['meanings'][j]['definitions'][k]['definition']
                             if 'synonyms' in definitionData[i]['meanings'][j]['definitions'][k] and len(definitionData[i]['meanings'][j]['definitions'][k]['synonyms']) > 0:
-                                returnData = returnData + '\n`'
                                 for l in range(len(definitionData[i]['meanings'][j]['definitions'][k]['synonyms'])):
-                                    returnData = returnData + ',' + definitionData[i]['meanings'][j]['definitions'][k]['synonyms'][l]
-                                returnData = returnData + '`'
+                                    synonymData = synonymData + ',' + definitionData[i]['meanings'][j]['definitions'][k]['synonyms'][l]
+                synonymData = synonymData.lstrip(',') + '`'
+                definitionData = definitionData.lstrip(' ')
+            returnData = returnData + '\n' + definitionData + '\n' + synonymData
     return returnData
