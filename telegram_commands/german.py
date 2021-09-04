536e2d2bd09c8508dade3b000927c59ccb6f093a
Ashley Lewis <ultrakiff@gmail.com>
2021-09-04 15:45:41 +0200

    patch for error: "ImportError No module named getgerman"

--- telegram_commands/german.py
+++ telegram_commands/german.py
@@ -1,3 +1,4 @@
-import telegram_commands.getgerman as getgerman
+import main
+getgerman = main.get_platform_command_code('telegram', 'getgerman')
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     getgerman.run(bot, chat_id, user, keyConfig, message, totalResults)
