5cae073acb9874ada4b8bf79b25ca217b09f74a1
Ashley Lewis <ultrakiff@gmail.com>
2022-06-09 18:51:24 +0200

    Update cric.py

--- telegram_commands/cric.py
+++ telegram_commands/cric.py
@@ -8,28 +8,5 @@ def run(bot, chat_id, user, keyConfig, message, totalResults=1):
 
 def get_cric_data(user):
     allMatchesUrl = 'http://cricscore-api.appspot.com/csa'
-    getData = urllib.urlopen(allMatchesUrl).read().decode('utf-8')
-    if ('<blockquote>' not in getData):
-        allMatches = json.loads(getData)
-        proteasMatchId = None
-        for match in allMatches:
-            if match['t1'] == 'South Africa' or match['t2'] == 'South Africa':
-                proteasMatchId = match['id']
-        if proteasMatchId == None:
-            return 'I\'m sorry ' + (user if not user == '' else 'Dave') + ', I\'m afraid the Proteas are not playing right now.'
-        else:
-            matchesUrl = 'http://cricscore-api.appspot.com/csa?id=' + str(proteasMatchId)
-            raw_data = urllib.urlopen(matchesUrl)
-            try:
-                match = json.load(raw_data)
-            except ValueError:
-                return 'Invalid response from api: ' + matchesUrl
-            return match[0]['si'] + '\n' + match[0]['de']
-    else:
-        return 'I\'m sorry ' + (user if not user == '' else 'Dave') + \
-               getData[getData.index('<blockquote>') + len('<blockquote>'):getData.index('</blockquote>')]\
-                   .replace('<H1>', '*')\
-                   .replace('</H1>', '*')\
-                   .replace('<p>', '')\
-                   .replace('</p>', '')
+    return 'I\'m sorry ' + (user if not user == '' else 'Dave') + ', I\'m afraid the /cric command is broken right now because ' + allMatchesUrl + ' is down.'
 
